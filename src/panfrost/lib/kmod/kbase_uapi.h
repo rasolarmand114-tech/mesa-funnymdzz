83ee74aa5f40d7b4b39327c6a4e6e7a89095f9a4
PM99 <rasolarmand114@gmail.com>
2026-09-06 23:19:29 +0330

    Update kbase_uapi.h

--- src/panfrost/lib/kmod/kbase_uapi.h
+++ src/panfrost/lib/kmod/kbase_uapi.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- *
+ *kbase_uapi.h
  * (C) COPYRIGHT 2017-2021 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
