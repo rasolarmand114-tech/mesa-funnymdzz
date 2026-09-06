65c86594dcc62c689c6c3a89dfca0ee0dfacd619
PM99 <rasolarmand114@gmail.com>
2026-09-06 23:24:07 +0330

    Update mali_kbase_ioctl.h

--- src/panfrost/lib/kmod/mali_kbase_ioctl.h
+++ src/panfrost/lib/kmod/mali_kbase_ioctl.h
@@ -1,6 +1,6 @@
 /* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
 /*
- *
+ *kmod/mali_kbase_ioctl.h
  * (C) COPYRIGHT 2017-2024 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
