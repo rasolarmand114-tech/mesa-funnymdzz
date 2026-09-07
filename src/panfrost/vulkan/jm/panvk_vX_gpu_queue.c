450b635d7f651d908c487cbe0a5560f71f712760
PM99 <rasolarmand114@gmail.com>
2026-09-07 19:40:00 +0330

    Update panvk_vX_gpu_queue.c

--- src/panfrost/vulkan/jm/panvk_vX_gpu_queue.c
+++ src/panfrost/vulkan/jm/panvk_vX_gpu_queue.c
@@ -25,7 +25,6 @@
 #include "panvk_image.h"
 #include "panvk_image_view.h"
 #include "panvk_instance.h"
-#include "panvk_kbase_jm.h"
 #include "panvk_physical_device.h"
 #include "panvk_priv_bo.h"
 #include "panvk_queue.h"
